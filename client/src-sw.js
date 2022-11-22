00ba244963106f1ad31d837b65f459fbdcdbbf32
nofutofu <aaronrweis@gmail.com>
2022-11-22 17:55:19 -0600

    finalized database and service worker / fixed error in webpack

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { StaleWhileRevalidate, CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -24,10 +24,20 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
-registerRoute(
-  ({ request }) => request.mode === 'navigate', pageCache
-  
-  );
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker',].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      })
+    ],
+  })
+);
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,25 +14,23 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
-  const textEditorDb = await openDB('textEditor', 1);
-  const tx = textEditorDb.transaction('textEditor', 'readwrite');
-  const store = tx.objectStore('textEditor');
-  const request = store.put({ id: id, text: content });
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, content: content });
   const result = await request;
   console.log('data has been saved to the DB', result);
 }
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
-  const textEditorDb = await openDB('textEditor', 1);
-  const tx = textEditorDb.transaction('textEditor', 'readonly');
-  const store = tx.objectStore('textEditor');
-  const request = store.getAll();
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result.content;
 }
 
 initdb();
